c3262ae399cb1fce16208fb440a7e8fbd0f15238
Lindberg Pereira <lindbergpg@gmail.com>
2024-07-20 11:47:55 -0300

    modifi personal tastes

--- trilhafrontend/src/components/PersonalTastes/index.tsx
+++ trilhafrontend/src/components/PersonalTastes/index.tsx
@@ -1,4 +1,4 @@
-import { Description, ContainerInfor, CardPersonalTastes, Cards } from './styles'
+import { Description, ContainerInfor, CardPersonalTastes, Cards, TitlePersonal } from './styles'
 import Title from '../Title'
 
 const PersonalTastes = () => (
@@ -10,6 +10,7 @@ const PersonalTastes = () => (
         <span className="material-symbols-outlined">
           music_note
         </span>
+        <TitlePersonal>Música eletrônica</TitlePersonal>
 				<Description>
         A música eletrônica me faz fugir da realidade, ela não serve só nos momentos de alegria, mas nos de tristeza também. Música eletrônica se vive.
 				</Description>
@@ -18,16 +19,18 @@ const PersonalTastes = () => (
         <span className="material-symbols-outlined">
           fitness_center
         </span>
+        <TitlePersonal>Musculação</TitlePersonal>
 				<Description>
           Fazer exercícios físicos não fortalece somente o corpo, mas também a mente e a alma. É um momento só seu de investimento exclusivo em você!
 				</Description>
 			</CardPersonalTastes>
       <CardPersonalTastes>
       <span className="material-symbols-outlined">
-        podcasts
+        local_cafe
       </span>
+      <TitlePersonal>Café</TitlePersonal>
 				<Description>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, perferendis omnis dolore sunt nobis veritatis incidunt quaerat amet magni dicta ipsum id dolor adipisci esse accusamus distinctio ex, eligendi quidem?
+        Um café bem feito é uma obra de arte, assim como uma vida bem vivida. O café é a bebida que une as melhores amizades.
 				</Description>
 			</CardPersonalTastes>
       </Cards>
